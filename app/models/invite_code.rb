162819e16f10a39ba982c84a6a8ae0eb3afe8b8f
ankonym <ankonym@gmail.com>
2015-08-25 14:11:00 +0200

    Make sure codes can only be used once, fix validations
    We introduced a count on invite codes to make sure that (at the moment) codes can only be used once. (The code will also allow multi-use codes in the future.)
    
    Also, some of our validations weren't validating against the correct data, which is now fixed.

--- app/models/invite_code.rb
+++ app/models/invite_code.rb
@@ -3,11 +3,14 @@ require 'coupon_code'
 class InviteCode < CouchRest::Model::Base
   use_database 'invite_codes'
   property :invite_code, String, :read_only => true
+  property :invite_count, Integer, :default => 0, :accessible => true
+
   timestamps!
 
   design do
     view :by__id
     view :by_invite_code
+    view :by_invite_count
   end
 
   def initialize(attributes = {}, options = {})
